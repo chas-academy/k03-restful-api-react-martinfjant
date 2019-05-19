59bc1e6f45ee247cad2347c2b157fc27709e7493
martinfjant <martin@falkjohansson.se>
2019-05-19 12:12:03 +0200

    test deploy

--- api/routes/item.js
+++ api/routes/item.js
@@ -1,21 +1,14 @@
-const Router = require('koa-router')
-const router = new Router()
-const Controller = require("../controllers/item")
-const jwt = require("../jwt")
-
-
+const Router = require("koa-router");
+const router = new Router();
+const Controller = require("../controllers/item");
+const jwt = require("../jwt");
 
 // /ITEM
-router.get("/", Controller.findAll)
-router.post("/", jwt.jwt, Controller.createItem)
+router.get("/", Controller.findAll);
 // /ITEM/:ID
-//router.get("/:id", Controller.findItem)
-router.put("/:id", Controller.updateItem)
-router.delete("/:id", Controller.deleteItem)
 // /ITEM/:TAG
-router.get("/tag/:tag", Controller.findByTag)
+router.get("/tag/:tag", Controller.findByTag);
 
-router.get("/test", Controller.testController)
+router.get("/test", Controller.testController);
 
-
-module.exports = router.routes()
+module.exports = router.routes();
